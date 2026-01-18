8765f35ead4950a45bca7a502774e4e40bd8706b
rizabyt-dot <rizabyt@gmail.com>
2026-01-18 07:45:38 +0530

    Create movie_bot.py

---
+++ movie_bot.py
@@ -0,0 +1,22 @@
+import requests
+
+API_KEY = "PASTE_YOUR_OMDB_API_KEY_HERE"
+
+def get_movie(movie_name):
+    url = f"http://www.omdbapi.com/?t={movie_name}&apikey={API_KEY}"
+    response = requests.get(url)
+    data = response.json()
+
+    if data.get("Response") == "True":
+        print("\n🎬 Movie Found!")
+        print("Title:", data["Title"])
+        print("Year:", data["Year"])
+        print("IMDb Rating:", data["imdbRating"])
+        print("Director:", data["Director"])
+        print("Actors:", data["Actors"])
+        print("Plot:", data["Plot"])
+    else:
+        print("❌ Movie not found")
+
+movie = input("Enter movie name: ")
+get_movie(movie)
